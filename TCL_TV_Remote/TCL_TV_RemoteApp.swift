dc728e2b5f6f0602f0652cc7b676831198b85a6a
bhution <bhushanbpatil8@gmail.com>
2026-07-04 13:40:18 -0500

    Initial Commit

---
+++ TCL_TV_Remote/TCL_TV_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCL_TV_RemoteApp.swift
+//  TCL_TV_Remote
+//
+//  Created by Bhushan Patil on 04/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TCL_TV_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
